2654e0acc92704f32f819bc71cb117e16161ab8f
Martin Behrendts <info@novusidea.de>
2025-12-20 10:39:05 +0000

    Updated for new syntax

--- resources/views/components/flyout.blade.php
+++ resources/views/components/flyout.blade.php
@@ -1,3 +1,3 @@
-<flux:modal {{ $attributes }} variant="flyout" class="max-lg:!ml-0 min-w-full lg:min-w-3/5 xl:min-w-2/5 space-y-6">
+<flux:modal {{ $attributes }} variant="flyout" class="max-lg:ml-0! min-w-full lg:min-w-3/5 xl:min-w-2/5 space-y-6">
     {{ $slot }}
 </flux:modal>
