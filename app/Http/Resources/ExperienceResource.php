5b3883d949f8a19a18972c847692ee6d760dda35
Martin Behrendts <info@novusidea.de>
2025-12-22 11:04:46 +0000

    Return unformatted dates

--- app/Http/Resources/ExperienceResource.php
+++ app/Http/Resources/ExperienceResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,24 +14,14 @@ class ExperienceResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $entry = $this->entry;
-        $exit = $this->exit;
-
-        $dateFormat = $request->string('date_format', 'm/Y');
-
-        if ($dateFormat !== 'raw') {
-            $entry = $entry?->format($dateFormat);
-            $exit = $exit?->format($dateFormat);
-        }
-
         return [
             'id' => $this->id,
             'position' => $this->position,
             'institution' => $this->institution,
             'location' => $this->location,
             'type' => $this->type,
-            'entry' => $entry,
-            'exit' => $exit,
+            'entry' => $this->entry,
+            'exit' => $this->exit,
             'duration' => $this->duration,
             'skills' => SkillResource::collection($this->skills),
             'description' => $this->description,
