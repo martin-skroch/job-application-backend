8633923ef0254338554f8eb501e2aa68e54b4b61
Martin Behrendts <info@novusidea.de>
2026-02-06 18:19:35 +0000

    Activated methdo for  job application requests

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApplicationsController;
 
 Route::group(['as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
+    Route::post('application/request', [ApplicationsController::class, 'request'])->name('application.request');
     Route::get('application/{application}', [ApplicationsController::class, 'fetch'])->name('application');
-    // Route::post('application/request', [ApplicationsController::class, 'request'])->name('application.request');
 });
