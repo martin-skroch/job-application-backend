0db61265731ecec45c97d2df629a7221af2730d9
Martin Behrendts <info@novusidea.de>
2026-02-06 09:29:06 +0000

    Activated ProfileSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // ProfileSeeder::class,
+            ProfileSeeder::class,
             ApplicationSeeder::class,
         ]);
     }
