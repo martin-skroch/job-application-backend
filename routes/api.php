cbae5ad5e0226f5a46bf039eece28ada4fbe6c36
Martin Behrendts <info@novusidea.de>
2026-02-06 09:57:52 +0000

    Removed unused use statements

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\SkillsController;
-use App\Http\Controllers\Api\ProfilesController;
-use App\Http\Controllers\Api\ExperiencesController;
 use App\Http\Controllers\Api\ApplicationsController;
 
 Route::group(['as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
